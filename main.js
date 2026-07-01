cba858f623fbf2ec74f9ffb992836c3ddce37f20
lucasdograau <lucas.martins.pereira11@escola.pr.gov.br>
2026-07-01 08:37:31 -0300

    ju

--- main.js
+++ main.js
@@ -16,16 +16,16 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2027-6-12");
-const tempoObjetivo1 = new Date("2027-2-10");
-const tempoObjetivo1 = new Date("2027-3-10");
-const tempoObjetivo1 = new Date("2027-8-10");
+const tempoObjetivo2 = new Date("2027-2-10");
+const tempoObjetivo3 = new Date("2027-3-10");
+const tempoObjetivo4 = new Date("2027-8-10");
 
 const tempos = [tempoObejetivo1, tempoObejetivo2, tempoObejetivo3, tempoObejetivo4];
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
-contadores[1].textContent = calculaTempo(tempoObjetivo1);
-contadores[2].textContent = calculaTempo(tempoObjetivo1);
-contadores[3].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
+contadores[3].textContent = calculaTempo(tempoObjetivo4);
 
 function calculaTempo(tempoObjetivo) {
   let tempoAtual = new Date();
